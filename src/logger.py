e1f7f659a1c81a77969baf43a617b4e41b4eb3a1
saikiranvankudothu <saikiran.vankudothu2004@gmail.com>
2025-01-08 16:47:40 +0530

    continue to Ingestoin

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,7 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%d-%m-%Y_%H_%M_%S')}.log"
+LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
@@ -10,9 +10,7 @@ LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format = "[%(asctime)s] [%(lineno)d %(name)s- %(levelname)s- %(message)s",
+    format = "[%(asctime)s] %(lineno)d %(name)s- %(levelname)s- %(message)s",
     level=logging.INFO,
+    
                     )
-
-if __name__ == '__main__':
-    logging.info("Logging as started")
